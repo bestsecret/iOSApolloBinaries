c1b9a778f351161aee1c50ca43e131cd7610eee0
Juan Pablo González <juanpablo.gonzalez@bestsecret.com>
2023-11-15 16:12:12 +0100

    [feat] Update package

--- Package.swift
+++ Package.swift
@@ -13,23 +13,10 @@ let package = Package(
       name: "ApolloLegacy",
       targets: ["ApolloLegacy"]),
     .library(
-        name: "ApolloLegacy-Dynamic",
-        type: .dynamic,
-        targets: ["ApolloLegacy"]),
-    .library(
       name: "ApolloLegacyCodegenLib",
       targets: ["ApolloLegacyCodegenLib"]),
-    .library(
-        name: "ApolloLegacySQLite",
-        targets: ["ApolloLegacySQLite"]),
-    .library(
-        name: "ApolloLegacyWebSocket",
-        targets: ["ApolloLegacyWebSocket"]),
     ],
     dependencies: [
-    .package(
-      url: "https://github.com/stephencelis/SQLite.swift.git",
-      .upToNextMinor(from: "0.13.1")),
     .package(
       url: "https://github.com/daltoniam/Starscream",
       .upToNextMinor(from: "3.1.1")),
@@ -38,90 +25,22 @@ let package = Package(
       .upToNextMinor(from: "0.14.0")),
     ],
     targets: [
-      .target(
+    .target(
         name: "ApolloLegacyCore",
-        dependencies: []),
+        dependencies: [],
+        path: "ApolloCore"),
     .target(
       name: "ApolloLegacy",
       dependencies: [
         "ApolloLegacyCore",
-      ]),
+      ],
+      path: "Apollo"),
     .target(
       name: "ApolloLegacyCodegenLib",
       dependencies: [
         "ApolloLegacyCore",
         .product(name: "Stencil", package: "Stencil"),
-      ]),
-    .target(
-      name: "ApolloLegacySQLite",
-      dependencies: [
-        "ApolloLegacy",
-        .product(name: "SQLite", package: "SQLite.swift"),
-      ]),
-    .target(
-      name: "ApolloLegacySQLiteTestSupport",
-      dependencies: [
-        "ApolloLegacySQLite",
-        "ApolloLegacyTestSupport"
-      ]),
-    .target(
-      name: "ApolloLegacyWebSocket",
-      dependencies: [
-        "ApolloLegacy",
-        "ApolloLegacyCore",
-        .product(name: "Starscream", package: "Starscream"),
-      ]),
-    .target(
-      name: "ApolloLegacyTestSupport",
-      dependencies: [
-        "ApolloLegacy",
-      ]),
-    .target(
-      name: "GitHubAPI",
-      dependencies: [
-        "ApolloLegacy",
-      ]),
-    .target(
-      name: "StarWarsLegacyAPI",
-      dependencies: [
-        "ApolloLegacy",
-      ]),
-    .target(
-      name: "UploadLegacyAPI",
-      dependencies: [
-        "ApolloLegacy",
-      ]),
-    .testTarget(
-      name: "ApolloLegacyTests",
-      dependencies: [
-        "ApolloLegacyTestSupport",
-        "StarWarsLegacyAPI",
-        "UploadLegacyAPI"
-      ]),
-    .testTarget(
-      name: "ApolloLegacyCacheDependentTests",
-      dependencies: [
-        "ApolloLegacySQLiteTestSupport",
-        "StarWarsLegacyAPI",
-      ]),
-    .testTarget(
-      name: "ApolloLegacyCodegenTests",
-      dependencies: [
-        "ApolloLegacyTestSupport",
-        "ApolloLegacyCodegenLib"
-      ]),
-    .testTarget(
-      name: "ApolloSQLiteTests",
-      dependencies: [
-        "ApolloLegacySQLiteTestSupport",
-        "StarWarsLegacyAPI"
-      ]),
-    .testTarget(
-      name: "ApolloLegacyWebsocketTests",
-      dependencies: [
-        "ApolloLegacyWebSocket",
-        "ApolloLegacyTestSupport",
-        "StarWarsLegacyAPI",
-      ]),
+      ],
+      path: "ApolloCodegenLib")
     ]
 )
